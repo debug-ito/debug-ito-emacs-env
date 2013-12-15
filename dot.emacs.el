a6c522340817207a7587acc377aefe5fcd6d5ad5
Toshio Ito <debug.ito@gmail.com>
2013-12-15 11:33:12 +0900

    enable word-wrap in markdown mode.

--- dot.emacs.el
+++ dot.emacs.el
@@ -151,6 +151,7 @@
 ;;;;;;;;;;;;;;;;;;;; markdown mode
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 (add-hook 'markdown-mode-hook (lambda () (local-set-key (kbd "C-m") 'newline)))
+(add-hook 'markdown-mode-hook (lambda () (setq word-wrap t)))
 
 ;;;;;;;;;;;;;; (C)Perl mode
 ;; http://www.emacswiki.org/emacs/CPerlMode
