2b9cfefaeeb3fc54de8572253de06bb067c75223
Toshio Ito <debug.ito@gmail.com>
2014-07-06 17:57:26 +0900

    misc-func: count-words-region is now defined in Emacs.

--- misc-func.el
+++ misc-func.el
@@ -1,30 +1,25 @@
 ;;; Count words in a region
 ;; http://www.math.s.chiba-u.ac.jp/~matsu/lisp/emacs-lisp-intro-jp_14.html
-(defun count-words-region (beginning end)  
-  "Print number of words in the region."
-  (interactive "r")
-  (message "Counting words in region ... ")
-
-;;; 1. 適切な状況の設定
-  (save-excursion
-    (let ((count 0))
-      (goto-char beginning)
-
-;;; 2.  while loop を走らせる。
-      (while (and (< (point) end)
-                  (re-search-forward "\\w+\\W*" end t))
-        (setq count (1+ count)))
-
-;;; 3. ユーザにメッセージを送る
-      (cond ((zerop count)
-             (message
-              "The region does NOT have any words."))
-            ((= 1 count)
-             (message
-              "The region has 1 word."))
-            (t
-             (message
-              "The region has %d words." count))))))
+(unless (fboundp 'count-words-region)
+  (defun count-words-region (beginning end)  
+    "Print number of words in the region."
+    (interactive "r")
+    (message "Counting words in region ... ")
+    (save-excursion
+      (let ((count 0))
+        (goto-char beginning)
+        (while (and (< (point) end)
+                    (re-search-forward "\\w+\\W*" end t))
+          (setq count (1+ count)))
+        (cond ((zerop count)
+               (message
+                "The region does NOT have any words."))
+              ((= 1 count)
+               (message
+                "The region has 1 word."))
+              (t
+               (message
+                "The region has %d words." count)))))))
 
 
 ;; Context-aware window switching
