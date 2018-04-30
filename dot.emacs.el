297d56533c5888930eb76f1643d18c0e2eadc2bd
Toshio Ito <debug.ito@gmail.com>
2018-04-30 21:56:01 +0900

    dot.emacs: set custom-file
    to stop Custom from modifying dot.emacs.el directly.

--- dot.emacs.el
+++ dot.emacs.el
@@ -1,5 +1,7 @@
 ;; Save this file in ISO-2022-JP because it is the coding system for Anthy!
 (add-to-list 'load-path "~/.emacs.d/lib")
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
 (load-library "debugito")
 (setq debugito-frame-width 128)
 (setq debugito-frame-height 26)
